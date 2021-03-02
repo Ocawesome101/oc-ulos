56a532d91707a6155d97db2889101f687a25f655
Ocawesome101 <ocawesome101@gmail.com>
2021-03-02 11:56:24 -0500

    updates

--- out/sbin/init.lua
+++ out/sbin/init.lua
@@ -7,10 +7,10 @@ local rf = {}
 do
   rf._NAME = "Refinement"
   rf._RELEASE = "0"
-  rf._RUNNING_ON = "ULOS 21.02-r0"
+  rf._RUNNING_ON = "ULOS 21.03-r0"
   
   io.write("\n  \27[97mWelcome to \27[93m", rf._RUNNING_ON, "\27[97m!\n\n")
-  local version = "2021.02.21"
+  local version = "2021.03.02"
   rf._VERSION = string.format("%s r%s-%s", rf._NAME, rf._RELEASE, version)
 end
 
@@ -79,8 +79,49 @@ rf.log(rf.prefix.done, "src/require")
 rf.log(rf.prefix.busy, "src/services")
 
 do
-  local process = require("process")
+  local svdir = "/etc/rf/"
+
   local running = {}
+  local sv = {up=true}
+  
+  local starting = {}
+  sv.up = function(srv)
+    checkArg(1, srv, "string")
+    if starting[srv] then
+      error("circular dependency detected")
+    end
+    local senv = setmetatable({needs=sv.up}, {__index=_G, __pairs=_G})
+    local spath = string.format("%s/%s", svdir, srv)
+    local ok, err = loadfile(svpath, nil, senv)
+    if not ok then
+      return nil, err
+    end
+    starting[srv] = true
+    local st, rt = pcall(ok)
+    if not st and rt then return nil, rt end
+    if senv.start then pcall(senv.start) end
+    running[srv] = senv
+    return true
+  end
+  
+  function sv.down(srv)
+    checkArg(1, srv, "string")
+    if not running[srv] then
+      return true, "no such service"
+    end
+    if running[srv].stop then
+      pcall(running[srv].stop)
+    end
+    running[srv] = nil
+  end
+  
+  function sv.msg(srv, ...)
+    checkArg(1, srv, "string")
+    if running[srv] and running[srv].msg then
+      return pcall(running[srv].msg, ...)
+    end
+    return true
+  end
 end
 
 rf.log(rf.prefix.done, "src/services")
