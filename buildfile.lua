eefc54cd30966bcb7f95db059eb3816039994a79
ocawesome101 <ocawesome101@gmail.com>
2021-06-03 20:44:50 -0400

    added help options to buildfile

--- buildfile.lua
+++ buildfile.lua
@@ -12,7 +12,6 @@ local extern = {
   "coresvc",
   "corelibs",
   "coreutils",
-  "manpages"
 }
 
 local build = function(dir)
@@ -23,6 +22,19 @@ end
 
 _G.main = function(args)
   for k,v in pairs(args) do args[v] = true end
+  if args.help or args["--help"] then
+    io.stderr:write("\
+usage: \27[96mbuild \27[93mOPTIONS\27[39m\
+Assembles ULOS.  \27[93mOPTIONS\27[39m should not be prefixed with a \27[91m--\27[39m.\
+\
+Available \27[93mOPTIONS\27[39m:\
+  \27[33mnomanual\27[39m:   do not include manual pages in the build.  reduces output size by about 100KB.\
+  \27[33mrelease\27[39m:    create a bootable MTAR archive (release image)\
+  \27[33mhelp\27[39m:       display this help.\
+  \27[33mocvm\27[39m:       automatically execute 'ocvm ..' when the build is complete.  used for my personal development setup.\
+")
+    os.exit(1)
+  end
   log("err", "Assembling ULOS")
   for _, dir in ipairs(seq) do
     build(dir)
@@ -31,6 +43,10 @@ _G.main = function(args)
   ex("mkdir -p out/sbin")
   ex("cp cynosure/kernel.lua out/init.lua")
   ex("cp refinement/refinement.lua out/sbin/init.lua")
+  if not args.nomanual then
+    extern[#extern+1] = "manpages"
+  end
+
   for _, file in ipairs(extern) do
     ex("cp -rv", "external/"..file.."/*", "out/")
   end
