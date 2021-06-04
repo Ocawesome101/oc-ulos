15ae3e2b2528616fb8ac4bb4f79c6f5b7ea9c9df
ocawesome101 <ocawesome101@gmail.com>
2021-06-03 23:17:29 -0400

    updates

--- buildfile.lua
+++ buildfile.lua
@@ -50,18 +50,19 @@ Available \27[93mOPTIONS\27[39m:\
   for _, file in ipairs(extern) do
     ex("cp -rv", "external/"..file.."/*", "out/")
   end
-  ex("cd tle; ./standalone.sh; cp tle ../out/bin/tle.lua")
+  ex("cd tle; ./standalone.lua; cp tle ../out/bin/tle.lua")
   ex("mkdir out/usr/share -p; cp -r tle/syntax out/usr/share/VLE")
   log("err", "ULOS assembled")
   if args.release then
     log("err, Creating MTAR archive")
     if os.getenv("TERM") == "cynosure" then
       ex("mtar --output=release.mtar (find out/)")
-      ex("into release.lua (cat cynosure/mtarldr.lua cynosure/mtarldr_2.lua release.mtar)")
+      ex("into -p release.lua cat cynosure/mtarldr.lua cynosure/mtarldr_2.lua release.mtar")
     else
       ex("find out -type f | utils/mtar.lua > release.mtar")
       ex("cat cynosure/mtarldr.lua release.mtar cynosure/mtarldr_2.lua > release.lua")
     end
+    os.remove("release.mtar")
   end
   if args.ocvm then
     os.execute("ocvm ..")
