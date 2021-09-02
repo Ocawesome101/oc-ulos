390888d24d3682bfcf3434d793d9426e7fe738ac
Ocawesome101 <ocawesome101@gmail.com>
2021-09-02 10:54:41 -0400

    updates

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 source utils/env.sh
 
 ogdir=$PWD
-external="cldr corelibs coreutils manpages upm uwm gpuproxy installer"
+external="cldr corelibs coreutils manpages upm uwm gpuproxy installer bsh"
 tobuild="cynosure external/usysd"
 
 build() {
