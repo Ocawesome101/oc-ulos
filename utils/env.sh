0564f55f761d3ef3315c896060ec15a37da43e1e
Ocawesome101 <ocawesome101@gmail.com>
2021-09-14 13:08:22 -0400

    updates

--- utils/env.sh
+++ utils/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export OS="ULOS"
 export DATE="$(date +%y.%m)"
-export ULOSREL="1.6"
+export ULOSREL="1.7"
 export ULOSVERSION="$OS $DATE-$ULOSREL"
 export PREPROCESSOR="$PWD/utils/proc.lua"
